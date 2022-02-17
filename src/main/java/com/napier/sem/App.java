7d2be4f53a3dda8c13a3769cc4fb5c71bffc0f41
pantheras <james34piper@googlemail.com>
2022-02-17 14:27:46 +0000

    edited docker file to run JAR

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -10,10 +10,6 @@ public class App
 
     public static void main(String[] args)
     {
-
-        System.out.println("Boo yah!");
-
-
         // Connect to MongoDB on local system - we're using port 27000
         MongoClient mongoClient = new MongoClient("mongo-dbserver");
         // Get a database - will create when we use it
